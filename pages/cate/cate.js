25107efae28bfbf9f2ee8ca4c6a386bba5599365
chenxi2015 <bbxycx.18@163.com>
2019-11-18 18:22:03 +0800

    首页

--- pages/cate/cate.js
+++ pages/cate/cate.js
@@ -1,4 +1,4 @@
-// pages/user/index.js
+// pages/cate/cate.js
 Page({
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,44 +4,43 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    bannerList: [],
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    var that = this;
+    wx.request({
+      url: app.data.url + '/index/index/index',
+      method: 'post',
+      data: {
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res.data);
+        var _data = res.data.data;
+        if(res.data.code == 0){
+          that.setData({
+            bannerList: _data.bannerList,
+
+          });
+        }else{
+          console.log('error')
         }
-      })
-    }
+
+
+        
+
+        
+
+      },
+      fail: function (e) {
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      },
+    })
   },
   getUserInfo: function(e) {
     console.log(e)
